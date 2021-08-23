9bf6188d1175ff58255c624269fb65ba43fe406a
Jeff Spooner <jdspoone@icloud.com>
2021-08-23 13:06:53 -0700

    Initial Commit

---
+++ XPCApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  XPCApp
+//
+//  Created by Jeff Spooner on 2021-08-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ XPCApp/XPCAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  XPCAppApp.swift
+//  XPCApp
+//
+//  Created by Jeff Spooner on 2021-08-23.
+//
+
+import SwiftUI
+
+@main
+struct XPCAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
